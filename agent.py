7022572371a083ab7ff4f4f74ec35890adbb3b69
Karthi <karthiamar07@gmail.com>
2026-08-11 11:36:21 +0530

    Add initial implementation of agent.py

---
+++ agent.py
@@ -0,0 +1,13 @@
+import os 
+from smolagents import CodeAgent , InferenceClientModel 
+
+model = InferenceClientModel(
+    model_id = "Qwen/Qwen2.5-72B-Instruct",
+    token=os.environ["HF_TOKEN"],
+)
+
+agent = CodeAgent(tools=[] , model=model , max_steps=4)
+
+result = agent.run("What is 17*4+9 ? Show your reasoning.")
+print("\n Final Answer")
+print(result)
