513977ff91a3b41d4f4cf0eda8c63ecdf9442d46
Karthi <karthiamar07@gmail.com>
2026-08-11 12:17:35 +0530

    Add DuckDuckGoSearchTool to CodeAgent

--- agent.py
+++ agent.py
@@ -1,13 +1,13 @@
 import os 
-from smolagents import CodeAgent , InferenceClientModel 
+from smolagents import CodeAgent , InferenceClientModel , DuckDuckGoSearchTool 
 
 model = InferenceClientModel(
     model_id = "Qwen/Qwen2.5-7B-Instruct",
     token=os.environ["HF_TOKEN"],
 )
 
-agent = CodeAgent(tools=[] , model=model , max_steps=4)
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()] , model=model , max_steps=4)
 
-result = agent.run("What is 17*4+9 ? Show your reasoning.")
+result = agent.run("What is the current version of python and when was is realesed")
 print("\n Final Answer")
 print(result)
