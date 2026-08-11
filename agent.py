8be782b0a3fab1fe4c118bd71df484f850d8a52f
Karthi <karthiamar07@gmail.com>
2026-08-11 15:03:10 +0530

    Add Tool import to agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,5 @@
 import os 
-from smolagents import CodeAgent , InferenceClientModel , DuckDuckGoSearchTool 
+from smolagents import CodeAgent , InferenceClientModel , DuckDuckGoSearchTool , Tool
 
 
 class SaveToFileTool(Tool):
